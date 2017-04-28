7161218dcc898a0a7195c1657f9d8cee571c6fc3
Christian Federmann <cfedermann@gmail.com>
2017-04-28 15:04:05 -0700

    Update initialize-ubuntuvm.sh

--- initialize-ubuntuvm.sh
+++ initialize-ubuntuvm.sh
@@ -2,17 +2,17 @@
 
 ### AppraiseOnAzure Ubuntu VM init script ###
 sudo apt-get update
-sudo apt-get update -y
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 sudo apt-get autoremove
 
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install unixodbc-dev
+sudo apt-get -y install lighttpd
+
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install Django==1.11
-sudo apt-get install unixodbc-dev
 sudo python3 -m pip install pyodbc
 sudo python3 -m pip install django-pyodbc-azure==1.11.0.0
-sudo apt-get install lighttpd
 
 sudo shutdown -r now
